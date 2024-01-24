0dabf32bf5f05b60077bcaeac39d3121d198e1e6
raja9112 <rrn.rajarajan>
2024-01-24 23:15:49 +0530

    66. Plus One

---
+++ 66. Plus One.py
@@ -0,0 +1,50 @@
+# Python's map() is a built-in function that allows you to process and transform all 
+# the items in an iterable without using an explicit for loop
+# Map in Python is a function that works as an iterator to return a result after applying a function to every item of an iterable (tuple, lists, etc.). 
+# It is used when you want to apply a single transformation function to all the iterable elements.
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        # Coverrting list to int
+        # Using map() and join() methods
+        # n = int(''.join(map(str, digits)))
+
+        n = int("".join(str(i) for i in digits)) + 1
+        List1 = list(map(int, str(n)))
+        return List1
+
+
+# You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+# Increment the large integer by one and return the resulting array of digits.
+
+ 
+
+# Example 1:
+
+# Input: digits = [1,2,3]
+# Output: [1,2,4]
+# Explanation: The array represents the integer 123.
+# Incrementing by one gives 123 + 1 = 124.
+# Thus, the result should be [1,2,4].
+# Example 2:
+
+# Input: digits = [4,3,2,1]
+# Output: [4,3,2,2]
+# Explanation: The array represents the integer 4321.
+# Incrementing by one gives 4321 + 1 = 4322.
+# Thus, the result should be [4,3,2,2].
+# Example 3:
+
+# Input: digits = [9]
+# Output: [1,0]
+# Explanation: The array represents the integer 9.
+# Incrementing by one gives 9 + 1 = 10.
+# Thus, the result should be [1,0].
+ 
+
+# Constraints:
+
+# 1 <= digits.length <= 100
+# 0 <= digits[i] <= 9
+# digits does not contain any leading 0's.
