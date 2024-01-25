27845ff950a8a64cc3260888fc73a5e742bb04a6
raja9112 <rrn.rajarajan>
2024-01-25 08:54:17 +0530

    70. Climbing Stairs

---
+++ 70. Climbing Stairs.py
@@ -0,0 +1,44 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # Explanation with image is in leetcode hint.
+        if n <= 3:
+            return n
+        n1, n2 = 2, 3
+
+        for i in range(4, n+1):
+            temp = n1 + n2
+            n1 = n2
+            n2 = temp
+        return n2
+        
+        # Recursion
+        # if n == 0 or n == 1:
+        #     return 1
+        # return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        
+# You are climbing a staircase. It takes n steps to reach the top.
+
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+# Example 1:
+
+# Input: n = 2
+# Output: 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+# Example 2:
+
+# Input: n = 3
+# Output: 3
+# Explanation: There are three ways to climb to the top.
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
+ 
+
+# Constraints:
+
+# 1 <= n <= 45
