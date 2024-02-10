86d4278c8c1d60246316b6b9ab807ba2b4e49e45
raja9112 <rrn.rajarajan@gamil.com>
2024-02-11 04:20:15 +0530

    Updates search insert ques by binary search

--- 35. Search Insert Position.py
+++ 35. Search Insert Position.py
@@ -2,21 +2,42 @@ class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         if not nums:
             return  
-        if target in nums:
-            output=nums.index(target)
-            return output
-        else:
-            nums.append(target)
-            nums.sort()
-            output=nums.index(target)
-            return output
-        
-        # Method 2
+        # if target in nums:
+        #     output=nums.index(target)
+        #     return output
+        # else:
+        #     nums.append(target)
+        #     nums.sort()
+        #     output=nums.index(target)
+        #     return output
+
         # for i, num in enumerate(nums):
         #     if num >= target:
         #         return i
         # return len(nums)
-        
+
+
+        # Binary search
+        l = 0
+        r = len(nums) - 1
+
+        while l<=r:
+            mid = (l + r) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        n = len(nums) - 1
+        nums.append(None)
+        while n > l:
+            nums[n] = nums[n-1]
+            n -= 1
+        nums[l] = target
+        return l
         
 # Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
