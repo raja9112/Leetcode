d74ea173701e7ce6fdd7dd4c9cbbcff92e4fa91d
raja9112 <rrn.rajarajan@gamil.com>
2024-02-10 12:31:39 +0530

    Leetcode

--- 35. Search Insert Position.py
+++ 35. Search Insert Position.py
@@ -11,6 +11,13 @@ class Solution:
             output=nums.index(target)
             return output
         
+        # Method 2
+        # for i, num in enumerate(nums):
+        #     if num >= target:
+        #         return i
+        # return len(nums)
+        
+        
 # Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
 # You must write an algorithm with O(log n) runtime complexity.
