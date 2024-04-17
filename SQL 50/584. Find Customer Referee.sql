0e660be38b8bcf64e572c8712a89554f7330e041
raja9112 <rrn.rajarajan@gamil.com>
2024-04-17 13:53:00 +0530

    Level: EASY

---
+++ SQL 50/584. Find Customer Referee.sql
@@ -0,0 +1,55 @@
+-- 584. Find Customer Referee
+
+-- Table: Customer
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- | referee_id  | int     |
+-- +-------------+---------+
+-- In SQL, id is the primary key column for this table.
+-- Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+ 
+
+-- Find the names of the customer that are not referred by the customer with id = 2.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Customer table:
+-- +----+------+------------+
+-- | id | name | referee_id |
+-- +----+------+------------+
+-- | 1  | Will | null       |
+-- | 2  | Jane | null       |
+-- | 3  | Alex | 2          |
+-- | 4  | Bill | null       |
+-- | 5  | Zack | 1          |
+-- | 6  | Mark | 2          |
+-- +----+------+------------+
+-- Output: 
+-- +------+
+-- | name |
+-- +------+
+-- | Will |
+-- | Jane |
+-- | Bill |
+-- | Zack |
+-- +------+
+
+-- Write your MySQL query statement below
+select name from Customer where referee_id != 2 or referee_id is null;
+
+-- SELECT name
+-- FROM Customer
+-- WHERE COALESCE(referee_id,0) <> 2;
+
+-- here COALESCE is used to replace NULL values with zero before checking whether it is equal to 2 or not.
